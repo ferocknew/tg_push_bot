b77e146a4c5a4e8ef060c9257328c7c39b1b48b5
Joanh.Fu <ferock@gmail.com>
2020-09-23 15:05:36 +0800

    update code

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,6 +2,8 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
     async index() {
+        let userId = 11;
+        const user = await ctx.service.user.find(userId);
         this.ctx.body = 'Hello world';
     }
 }
---
+++ app/service/user.js
@@ -0,0 +1,7 @@
+class UserService extends Service {
+    async find(uid) {
+        // 假如 我们拿到用户 id 从数据库获取用户详细信息
+        const user = await this.app.mysql.get('users', {id: uid});
+        return {user};
+    }
+}
