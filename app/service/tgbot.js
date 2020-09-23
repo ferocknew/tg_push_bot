de5e7ef44edb3ce0a2997e17e5dd371129e53193
Joanh.Fu <ferock@gmail.com>
2020-09-23 19:15:45 +0800

    update code

--- app/service/tgbot.js
+++ app/service/tgbot.js
@@ -4,12 +4,16 @@ class TgbotService extends Service {
     async command(commandText) {
         const {ctx, app} = this;
         ctx.logger.info('TgbotService.command || commandText = %j', commandText);
+        console.info(2);
         await this[commandText]();
+        console.info(3);
     }
 
     async start() {
         const {ctx, app} = this;
+        console.info(1);
         ctx.logger.info('TgbotService.start || init');
+
     }
 }
 
