7c6e5e9c2022df0030e652a9791960d436f4d7c7
Joanh.Fu <ferock@gmail.com>
2020-09-23 15:08:36 +0800

    update code

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,7 +3,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
     async index() {
         let userId = 11;
-        const user = await ctx.service.user.find(userId);
+        // const user = await ctx.service.user.find(userId);
         this.ctx.body = 'Hello world';
     }
 }
