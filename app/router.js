ecfdb55677746c09589823d9d9bd9f971361d24a
Joanh.Fu <ferock@gmail.com>
2020-09-23 21:58:36 +0800

    update code

--- app/router.js
+++ app/router.js
@@ -3,10 +3,10 @@ module.exports = app => {
     router.get('/', controller.home.index);
     router.get('/inlineQuery', controller.home.inlineQuery);
     router.get('/sendMessage/:token', controller.home.sendMessage);
-    router.get('/sendPhoto/:token', controller.home.sendMessage);
+    router.get('/sendPhoto/:token', controller.home.sendPhoto);
     router.get('/test', controller.home.test);
 
     router.post('/inlineQuery', controller.home.inlineQuery);
     router.post('/sendMessage/:token', controller.home.sendMessage);
-    router.post('/sendPhoto/:token', controller.home.sendMessage);
+    router.post('/sendPhoto/:token', controller.home.sendPhoto);
 };
