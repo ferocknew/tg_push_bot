f04de87efde1118155a7f1032022af5f69f8d842
Joanh.Fu <ferock@gmail.com>
2020-09-24 11:43:42 +0800

    update code

--- config/config.default.js
+++ config/config.default.js
@@ -1,12 +1,10 @@
-exports.botUi = {
-    startMsg: "用户信息已登记，你的推送链接Token 为："
-};
-
 module.exports = {
     security: {
         csrf: {
             enable: false,
         },
+    }, botUi: {
+        startMsg: "用户信息已登记，你的推送链接Token 为："
     }
 };
 
