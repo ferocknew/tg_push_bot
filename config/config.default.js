9886c4dd64a1e846d038c2c7e652f3daa411c213
Joanh.Fu <ferock@gmail.com>
2020-09-24 11:30:06 +0800

    update code

--- config/config.default.js
+++ config/config.default.js
@@ -1,8 +1,13 @@
-exports.keys = "";
 module.exports = {
     security: {
         csrf: {
             enable: false,
         },
     },
+    bot: {
+        ui: {
+            startMsg: "用户信息已登记，你的推送链接Token 为："
+        }
+    }
 };
+
