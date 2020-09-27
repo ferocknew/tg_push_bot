743cdaaeacf3127f48f5b6bd92100949252b467b
Joanh.Fu <ferock@gmail.com>
2020-09-27 22:00:27 +0800

    update code

--- app/controller/home.js
+++ app/controller/home.js
@@ -133,7 +133,7 @@ class HomeController extends Controller {
                 method: 'pwg.session.getStatus'
             },
             headers: {
-                'Cookie': cookies.join("")
+                'Cookie': cookie.join("")
             },
             // 明确告诉 HttpClient 以 JSON 格式处理返回的响应 body
             dataType: 'json',
