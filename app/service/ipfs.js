9ce8b0c529c4763bf0a93e8058f31ff75c995c13
Joanh.Fu <ferock@gmail.com>
2020-09-28 13:08:20 +0800

    update code

--- app/service/ipfs.js
+++ app/service/ipfs.js
@@ -57,7 +57,7 @@ class IpfsService extends Service {
         try {
             // this.saveToCheveretoAPI(uri);
             // ctx.service.piwigo.uploadImg(saveFilePath);
-            ctx.service.piwigo.uploadImgFromUrl(uri);
+            ctx.service.piwigo.uploadImgFromUrl(uri, `${fileName}${extname}`);
 
         } catch (e) {
             ctx.logger.warn('IpfsService.saveUrl || e = %j', e);
--- app/service/piwigo.js
+++ app/service/piwigo.js
@@ -91,46 +91,51 @@ class PiwigoService extends Service {
         return true;
     }
 
-    async uploadImgFromUrl(uri) {
+    async uploadImgFromUrl(uri, fileName) {
         const {ctx, app} = this;
         let cheveretoConfig = app.config.chevereto;
         let ownHost = cheveretoConfig.ownHost;
         let picUrl = `${ownHost}${uri}`;
         ctx.logger.info('PiwigoService.uploadImgFromUrl || url = %j', picUrl);
 
-        //
+        let token = await this.getToken();
+        let cookieString = this.cookie;
+        ctx.logger.info('PiwigoService.uploadImgFromUrl || token = %j', token);
+        ctx.logger.info('PiwigoService.uploadImgFromUrl || cookieString = %j', cookieString);
         let piwigoConfig = app.config.piwigo;
         let url = piwigoConfig.url;
-        const result = await ctx.curl(url, {
-            // 必须指定 method
-            method: 'POST',
-            // 通过 contentType 告诉 HttpClient 以 JSON 格式发送
-            // contentType: 'json',
-            data: {
-                method: 'pwg.session.login',
-                username: piwigoConfig.userName,
-                password: piwigoConfig.password
-            },
-            // 明确告诉 HttpClient 以 JSON 格式处理返回的响应 body
-            dataType: 'json',
-        });
-        let cookie = result.headers['set-cookie'];
+        // const result = await ctx.curl(url, {
+        //     // 必须指定 method
+        //     method: 'POST',
+        //     // 通过 contentType 告诉 HttpClient 以 JSON 格式发送
+        //     // contentType: 'json',
+        //     data: {
+        //         method: 'pwg.session.login',
+        //         username: piwigoConfig.userName,
+        //         password: piwigoConfig.password
+        //     },
+        //     timeout: this.timeout,
+        //     // 明确告诉 HttpClient 以 JSON 格式处理返回的响应 body
+        //     dataType: 'json',
+        // });
+        // let cookie = result.headers['set-cookie'];
         // ctx.logger.info('PiwigoService.uploadImgFromUrl  || cookie = %j', cookie);
-        let res = await ctx.curl(url, {
-            // 必须指定 method
-            method: 'POST',
-            // 通过 contentType 告诉 HttpClient 以 JSON 格式发送
-            // contentType: 'json',
-            data: {
-                method: 'pwg.session.getStatus'
-            },
-            headers: {
-                'Cookie': cookie.join("")
-            },
-            // 明确告诉 HttpClient 以 JSON 格式处理返回的响应 body
-            dataType: 'json',
-        });
-        let token = res.data.result.pwg_token;
+        // let res = await ctx.curl(url, {
+        //     // 必须指定 method
+        //     method: 'POST',
+        //     // 通过 contentType 告诉 HttpClient 以 JSON 格式发送
+        //     // contentType: 'json',
+        //     data: {
+        //         method: 'pwg.session.getStatus'
+        //     },
+        //     headers: {
+        //         'Cookie': cookie.join("")
+        //     },
+        //     timeout: this.timeout,
+        //     // 明确告诉 HttpClient 以 JSON 格式处理返回的响应 body
+        //     dataType: 'json',
+        // });
+        // token = res.data.result.pwg_token;
         // ctx.logger.info('PiwigoService.uploadImgFromUrl  || token = %j', token);
         let category = piwigoConfig.categorieId;
         // let fileRes = await app.curl("https://hashnews.k1ic.com/ipfs/QmZkGfrbgguZ2vsBNgKEP3ctoz5iKxUDETL24RDBivEgKC/o4wz6hm1dqzkfl3ogus.jpg");
@@ -146,17 +151,17 @@ class PiwigoService extends Service {
                 method: 'pwg.images.upload',
                 category: category,
                 pwg_token: token,
-                name: "o4wz6hm1dqzkfl3nr09.jpg",
+                name: fileName,
                 source: picUrl
                 // file: fs.createReadStream(filePath)
             },
+            timeout: this.timeout,
             // files: filePath,
             headers: {
-                'Cookie': cookie.join("")
+                'Cookie': cookieString
             },
             // 明确告诉 HttpClient 以 JSON 格式处理返回的响应 body
             dataType: 'json',
-            timeout: 120000,
         });
         // res = await ctx.curl(url, {
         //     // 必须指定 method
