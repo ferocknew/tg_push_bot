34a1887dc55636921be34c58d0b643a8f62801b1
Joanh.Fu <ferock@gmail.com>
2020-10-16 18:30:34 +0800

    update code

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 module.exports = app => {
     const {router, controller} = app;
     router.get('/', controller.home.index);
+    router.get('/dplay', controller.home.dplay);
     router.get('/inlineQuery', controller.home.inlineQuery);
     router.get('/sendMessage/:token', controller.home.sendMessage);
     router.get('/sendPhoto/:token', controller.home.sendPhoto);
